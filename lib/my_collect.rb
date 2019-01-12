ac560ed783a05894c259cd73a2e0cbdde0dca8ab
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-12 03:59:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect (collection)
+  i = 0
+  temp = ""
+  new_collection = []
+  
+  while i < collection.length
+    temp = yield collection[i]
+    new_collection << temp
+    i += 1
+  end
+  
+  new_collection
+end
