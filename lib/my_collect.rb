1759297d7b598c4d3803acbec93fb6a8a3ed8823
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:20:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array) 
+  array.collect do |name| name.split(" ")
+ end
+   if array == "languages"
+   new_lang = []
+   array.map do |name| new_lang << name.upcase!
+end
 
-
+new_lang
+end
+end
