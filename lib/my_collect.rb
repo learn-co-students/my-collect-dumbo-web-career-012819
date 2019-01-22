ad6a07c4deb4beaa35a39d7e1b18b9dc2e856643
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-22 19:42:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array) 
   array.collect do |name| name.split(" ")
  end
-   if array == "languages"
+   while array == "languages"
    new_lang = []
-   array.map do |name| new_lang << name.upcase!
+   array.each do |name| new_lang << name.upcase!
 end
-
-new_lang
+yield new_lang
 end
 end
